05e1ce84608400489296a78f4a2f6c8a157107a2
Naveen M K <naveen521kk@gmail.com>
2022-12-07 22:47:34 +0530

    Update site url

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,7 +3,7 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Super Duper Blog`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://super-duper-blog.naveenmk.me/`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
