87ac5ba7ec9e517514b956108c623ec61cb35378
Naveen M K <naveen521kk@gmail.com>
2022-12-07 23:04:52 +0530

    Add modern-normalize
    As well, configure prettier

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,29 +1,38 @@
-import type { GatsbyConfig } from "gatsby";
+import type {GatsbyConfig} from "gatsby";
 
 const config: GatsbyConfig = {
-  siteMetadata: {
-    title: `Super Duper Blog`,
-    siteUrl: `https://super-duper-blog.naveenmk.me/`
-  },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
-  graphqlTypegen: true,
-  plugins: [`gatsby-plugin-sass`, "gatsby-plugin-image", "gatsby-plugin-sitemap", "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+    siteMetadata: {
+        title: `Super Duper Blog`,
+        siteUrl: `https://super-duper-blog.naveenmk.me/`
     },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
+    // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+    // If you use VSCode you can also use the GraphQL plugin
+    // Learn more at: https://gatsby.dev/graphql-typegen
+    graphqlTypegen: true,
+    plugins: [
+        `gatsby-plugin-sass`,
+        "gatsby-plugin-image",
+        "gatsby-plugin-sitemap",
+        "gatsby-plugin-mdx",
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "images",
+                path: "./src/images/"
+            },
+            __key: "images"
+        },
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "pages",
+                path: "./src/pages/"
+            },
+            __key: "pages"
+        }
+    ]
 };
 
 export default config;
