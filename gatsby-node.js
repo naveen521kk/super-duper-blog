c446d9a3cc15705b46c31fba72f6256ba49ba6c1
MrK619 <97443351+MrK619@users.noreply.github.com>
2022-12-09 19:26:21 +0530

    Add support for mdx

---
+++ gatsby-node.js
@@ -0,0 +1,43 @@
+const path = require("path")
+const postTemplate = path.resolve(`./src/templates/post.jsx`)
+
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions
+
+  const result = await graphql(`
+    query {
+      allMdx {
+        nodes {
+          id
+          frontmatter {
+            slug
+          }
+          internal {
+            contentFilePath
+          }
+        }
+      }
+    }
+  `)
+
+  if (result.errors) {
+    reporter.panicOnBuild('Error loading MDX result', result.errors)
+  }
+
+  // Create blog post pages.
+  const posts = result.data.allMdx.nodes
+
+  // you'll call `createPage` for each result
+  posts.forEach(node => {
+    createPage({
+      // As mentioned above you could also query something else like frontmatter.title above and use a helper function
+      // like slugify to create a slug
+      path: node.frontmatter.slug,
+      // Provide the path to the MDX content file so webpack can pick it up and transform it into JSX
+      component: `${postTemplate}?__contentFilePath=${node.internal.contentFilePath}`,
+      // You can use the values in this context in
+      // our page layout component
+      context: { id: node.id },
+    })
+  })
+}
---
+++ src/templates/post.jsx
@@ -0,0 +1,27 @@
+import React from "react"
+import { graphql } from "gatsby"
+import { MDXProvider } from "@mdx-js/react"
+import { Link } from "gatsby"
+
+const shortcodes = { Link } // Provide common components here
+
+export default function PageTemplate({ data, children }) {
+  return (
+    <>
+      <h1>{data.mdx.frontmatter.title}</h1>
+      <MDXProvider components={shortcodes}>
+        {children}
+      </MDXProvider>
+    </>
+  )
+}
+
+export const query = graphql`
+  query($id: String!) {
+    mdx(id: { eq: $id }) {
+      frontmatter {
+        title
+      }
+    }
+  }
+`
